ea3f5f49c37e902ecb71ab8da0800e251266373c
Tomek Kruszyna <tomek.kruszyna@gmail.com>
2020-04-23 16:03:30 +0200

    Liczby parzyste i suma

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+                sum += i;
+            }
+        }
+        System.out.println("Suma liczb parzystych z zakresu 0-100: " + sum);
+    }
+}
